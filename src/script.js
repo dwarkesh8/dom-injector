c523ba16bb18694a883fcca72d3d555f4994350e
Circle-layer-org <dwarkeshpurohit8@gmail.com>
2025-08-12 22:11:33 +0530

    removed src and build files

--- src/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('surveyForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const name = this.name.value;
-  const rating = this.rating.value;
-  document.getElementById('response').textContent =
-    `Thank you ${name}! You rated us "${rating}".`;
-  this.reset();
-});
